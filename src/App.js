2c9a5196164c19238dc053def3da6f493a5d7f88
Enjila <enjila.moktan@gmail.com>
2021-02-07 13:52:11 +1100

    reservation-screen

--- src/App.js
+++ src/App.js
@@ -1,52 +1,52 @@
 import logo from "./logo.svg";
 import "./App.css";
+import { Button, FormControl, InputGroup, Card } from "react-bootstrap"; 
+import "bootstrap/dist/css/bootstrap.min.css"
 
 export default function App() {
   return (
     <div>
-      <section id="colorlib-hero" className="js-fullheight" data-section="home">
-        <div className="flexslider js-fullheight">
-          <ul className="slides">
-            <li>
-              <div className="overlay" />
-              <div className="container-fluid">
-                <div className="row">
-                  <div className="col-md-6 col-md-offset-3 col-md-pull-3 col-sm-12 col-xs-12 js-fullheight slider-text">
-                    <div className="slider-text-inner js-fullheight">
-                      <div className="desc">
-                        <h1>
-                          <a href="" target="_blank">
-                            Search
-                          </a>
-                        </h1>
-                        <h1>Career Pathway</h1>
-                        <h1>
-                          <a href="" target="_blank">
-                            Search your roles for quicker response
-                          </a>
-                        </h1>
-                      </div>
+      <section class="career-section">
+              <div class="container">
+              <div class="container-fluid">
+                <div class="row">
+                <div class="carrer-path-section">
+                <div class="row">
+                  <div class="col-lg-7 col-md-7 col-sm-12 col-xs-12">
+                  <h3 class= "career-title"> What is Lorem Ipsum?</h3>
+                  <p> 
+                  Lorem Ipsum is simply dummy text of the printing and typesetting industry. 
+                  Lorem Ipsum has been the industry </p>
+
+                 <a href="#" class="see-more">Reserve</a>
+                  </div>  
+                  {/* end of col */}
+                  <div class="col-lg-5 col-md-5 col-sm-12 col-xs-12">
+                    <img src="https://cdn.mentorcruise.com/img/undraw_unexpected_friends_tg6k.svg" alt="student image" class="img-fluid"></img>
                     </div>
+                  </div>  
                   </div>
+
+                  
+
+
+                  {/* end of career path */}
+                  </div> 
+
+                  {/* end row */}
+                   
+
                 </div>
-                <div className="row">
-                  <div className="col-md-6 col-md-offset-3 col-md-pull-3 col-sm-12 col-xs-12 js-fullheight slider-text">
-                    <div className="slider-text-inner js-fullheight">
-                      <div className="desc">
-                        <h2>Tell me about yourself</h2>
-                        <h5>Experience Level</h5>
-                        <h5>Degree</h5>
-                        <h5>Skills</h5>
-                        <h5>Work Rights</h5>
-                        <h5>Location</h5>
-                      </div>
-                    </div>
-                  </div>
+
+                {/* container fluid */}
                 </div>
-              </div>
-            </li>
-          </ul>
-        </div>
+
+                {/* container */}
+
+
+             
+           
+        
       </section>
     </div>
   );
